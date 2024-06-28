e332cb27cfd33fd9eec5f4437cc331d68a91ecb3
Ricardo Medina Sterling <ricardoms1909@gmail.com>
2024-06-28 01:30:51 -0500

    refactor: update component imports in page files

--- src/app/(auth)/oauth/page.tsx
+++ src/app/(auth)/oauth/page.tsx
@@ -1,80 +1,84 @@
-"use client"
-
-import { Typography, Link } from '@mui/material';
-import { LoginForm } from '@/components';
-import { useEffect, useState } from 'react';
-import { useRouter } from 'next/navigation';
-import { useDispatch } from 'react-redux';
-import { setUser } from '@/store';
+'use client';
+import { useEffect } from 'react';
 import NextLink from 'next/link';
+import { useRouter } from 'next/navigation';
+import { Typography, Link } from '@mui/material';
+import { useDispatch } from 'react-redux';
+
+import { setUser } from '@/store';
+import { LoginForm } from '@/components';
 
 const OauthPage = () => {
-  const router = useRouter();
-  const dispatch = useDispatch();
+	const router = useRouter();
+	const dispatch = useDispatch();
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const queryParams = new URLSearchParams(window.location.search);
-        const params = Object.fromEntries(queryParams.entries());
-        localStorage.setItem('token', params.token);
-        console.log(params);
-        const response = await fetch((process.env.BACKEND_URL || "http://localhost:3000") + "/user/status", {
-          method: 'GET',
-          headers: {
-            'Authorization': `Bearer ${params.token}`,
-            'Content-Type': 'application/json'
-          }
-        });
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const queryParams = new URLSearchParams(window.location.search);
+				const params = Object.fromEntries(queryParams.entries());
+				localStorage.setItem('token', params.token);
+				console.log(params);
+				const response = await fetch(
+					(process.env.BACKEND_URL || 'http://localhost:3000') +
+						'/user/status',
+					{
+						method: 'GET',
+						headers: {
+							Authorization: `Bearer ${params.token}`,
+							'Content-Type': 'application/json',
+						},
+					}
+				);
 
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
+				if (!response.ok) {
+					throw new Error('Network response was not ok');
+				}
 
-        const result = await response.json();
-        dispatch(setUser({ user: result }));
-        router.push('/');
-      } catch (error) {
-        console.log(error);
-      }
-    };
+				const result = await response.json();
+				dispatch(setUser({ user: result }));
+				router.push('/');
+			} catch (error) {
+				console.log(error);
+			}
+		};
 
-    fetchData();
-  }, [dispatch, router]);
+		fetchData();
+	}, [dispatch, router]);
 
-  return (
-    <>
-      <Typography
-        component="h1"
-        variant="h5"
-        textAlign="center"
-        gutterBottom
-        sx={{ color: 'text.primary', fontWeight: 'bold', mb: 2 }}
-      >
-        Iniciar Sesión
-      </Typography>
-      <LoginForm />
-      <Typography
-        variant="body2"
-        textAlign="center"
-        mt={2}
-        sx={{ color: 'text.primary' }}
-      >
-        ¿Aún no tienes cuenta?{' '}
-        <Link
-          href="/registrarse"
-          component={NextLink}
-          style={{
-            color: '#377AB8',
-            textDecoration: 'none',
-            fontWeight: 'bold',
-          }}
-        >
-          Crear cuenta
-        </Link>
-      </Typography>
-    </>
-  );
+	return (
+		<>
+			<Typography
+				component="h1"
+				variant="h5"
+				textAlign="center"
+				gutterBottom
+				sx={{ color: 'text.primary', fontWeight: 'bold', mb: 2 }}
+			>
+				Iniciar Sesión
+			</Typography>
+			<LoginForm />
+			<Typography
+				variant="body2"
+				textAlign="center"
+				mt={2}
+				sx={{ color: 'text.primary' }}
+			>
+				¿Aún no tienes cuenta?{' '}
+				<Link
+					href="/registrarse"
+					component={NextLink}
+					style={{
+						color: '#377AB8',
+						textDecoration: 'none',
+						fontWeight: 'bold',
+					}}
+				>
+					Crear cuenta
+				</Link>
+			</Typography>
+		</>
+	);
 };
 
 export default OauthPage;
--- src/components/forms/LoginForm.tsx
+++ src/components/forms/LoginForm.tsx
@@ -15,11 +15,13 @@ import {
 	InputAdornment,
 	IconButton,
 	Link,
+	Typography,
+	Divider,
 } from '@mui/material';
 import { Visibility, VisibilityOff } from '@mui/icons-material';
 import GoogleIcon from '@mui/icons-material/Google';
 import { LoginSchema } from '@/schemas';
-import { setUser, useLoginUserMutation} from '@/store';
+import { setUser, useLoginUserMutation } from '@/store';
 
 export const LoginForm = () => {
 	const {
@@ -59,15 +61,12 @@ export const LoginForm = () => {
 		}
 	}
 
-	async function onSubmitGoogleAuth(event: any ){ 
+	async function onSubmitGoogleAuth(event: any) {
 		try {
-
 			event.preventDefault();
 			if (typeof window !== 'undefined') {
 				window.location.href = `http://localhost:3000/auth/google/login`;
 			}
-
-			
 		} catch (error: any) {
 			if (error?.status === 401) {
 				setErrorMap('Email o contraseña incorrectos');
@@ -161,19 +160,6 @@ export const LoginForm = () => {
 				</Grid>
 				<Grid item xs={12}>
 					<Button
-						// type="submit"
-						fullWidth
-						variant="contained"
-						sx={{
-							marginBottom: '1rem'
-						}}
-						onClick={onSubmitGoogleAuth}
-						//disabled={!isDirty || isSubmitting}
-						//endIcon={isSubmitting && <CircularProgress size={20} />}
-					>
-						Conectarse con Google <GoogleIcon sx={{marginLeft: '0.5rem'}}/>
-					</Button>
-					<Button
 						type="submit"
 						fullWidth
 						variant="contained"
@@ -181,6 +167,25 @@ export const LoginForm = () => {
 						endIcon={isSubmitting && <CircularProgress size={20} />}
 					>
 						Iniciar Sesión
+					</Button>
+				</Grid>
+
+				<Grid item xs={12}>
+					<Divider sx={{ mb: 3 }}>O</Divider>
+
+					<Button
+						// type="submit"
+						fullWidth
+						variant="contained"
+						sx={{
+							marginBottom: '1rem',
+						}}
+						onClick={onSubmitGoogleAuth}
+						//disabled={!isDirty || isSubmitting}
+						//endIcon={isSubmitting && <CircularProgress size={20} />}
+					>
+						Conectarse con Google{' '}
+						<GoogleIcon sx={{ marginLeft: '0.5rem' }} />
 					</Button>
 				</Grid>
 			</Grid>
